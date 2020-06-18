2468506f18cb0a2c849a4b485ac3d9544d973de4
eMdOS <emilio.ojeda.mendoza@gmail.com>
2020-06-18 13:50:47 -0500

    feat: extend Diamond to provide "endofunction" composition

--- Sources/fswift/Diamond.swift
+++ Sources/fswift/Diamond.swift
@@ -1,6 +1,20 @@
 
 precedencegroup AssociativeComposition {
     associativity: left
+    higherThan: ForwardApplication
 }
 
 infix operator <> : AssociativeComposition
+
+public func <> <A>(f: @escaping (A) -> A, g: @escaping (A) -> A) -> (A) -> A {
+    return { a in
+        g(f(a))
+    }
+}
+
+public func <> <A>(f: @escaping (inout A) -> Void, g: @escaping (inout A) -> Void) -> (inout A) -> Void {
+    return { a in
+        f(&a)
+        g(&a)
+    }
+}
