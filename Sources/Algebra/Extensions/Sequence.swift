03824502d7822581548f3f56dbdfe4815f6669c6
eMdOS <emilio.ojeda.mendoza@gmail.com>
2020-06-25 13:07:43 -0500

    feat: add the "And" and "Or" predicates for filtering purposes

--- Sources/Algebra/Extensions/Sequence.swift
+++ Sources/Algebra/Extensions/Sequence.swift
@@ -3,4 +3,12 @@ public extension Sequence {
     func filter(by predicate: Predicate<Iterator.Element>) -> [Iterator.Element] {
         filter({ predicate.transform($0) })
     }
+
+    func filtered(by predicate: PredicateAnd<Iterator.Element>) -> [Iterator.Element] {
+        filter({ predicate.transform($0).unwrap })
+    }
+
+    func filtered(by predicate: PredicateOr<Iterator.Element>) -> [Iterator.Element] {
+        filter({ predicate.transform($0).unwrap })
+    }
 }
--- Sources/Algebra/Types/Predicates.swift
+++ Sources/Algebra/Types/Predicates.swift
@@ -1,2 +1,6 @@
 
 public typealias Predicate<A> = Function<A, Bool>
+
+public typealias PredicateAnd<A> = Function<A, And>
+
+public typealias PredicateOr<A> = Function<A, Or>
