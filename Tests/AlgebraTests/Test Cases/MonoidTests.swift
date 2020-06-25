f89bb2cfb156a2184d2c8cfcafedc23b5ab9685a
eMdOS <emilio.ojeda.mendoza@gmail.com>
2020-06-25 13:19:29 -0500

    test: verify "And" and "Or" type are Semigroups and Monoids

--- Tests/AlgebraTests/Test Cases/MonoidTests.swift
+++ Tests/AlgebraTests/Test Cases/MonoidTests.swift
@@ -17,4 +17,14 @@ final class MonoidTests: XCTestCase {
     func test_optional_isMonoid() {
         XCTAssert(Law<String?>.hasNeutralIdentity(a: "a"))
     }
+
+    func test_and_isMonoid() {
+        XCTAssert(Law<And>.hasNeutralIdentity(a: true))
+        XCTAssert(Law<And>.hasNeutralIdentity(a: false))
+    }
+
+    func test_or_isMonoid() {
+        XCTAssert(Law<Or>.hasNeutralIdentity(a: true))
+        XCTAssert(Law<Or>.hasNeutralIdentity(a: false))
+    }
 }
--- Tests/AlgebraTests/Test Cases/SemigroupTests.swift
+++ Tests/AlgebraTests/Test Cases/SemigroupTests.swift
@@ -17,4 +17,32 @@ final class SemigroupTests: XCTestCase {
     func test_optional_isSemigroup() {
         XCTAssert(Law<String?>.isAssociative(a: "a", b: "b", c: "c"))
     }
+
+    func test_and_isSemigroup() {
+        XCTAssert(Law<And>.isAssociative(a: true, b: true, c: true))
+        XCTAssert(Law<And>.isAssociative(a: false, b: false, c: false))
+
+        XCTAssert(Law<And>.isAssociative(a: true, b: true, c: false))
+        XCTAssert(Law<And>.isAssociative(a: true, b: false, c: false))
+
+        XCTAssert(Law<And>.isAssociative(a: false, b: true, c: true))
+        XCTAssert(Law<And>.isAssociative(a: false, b: true, c: false))
+
+        XCTAssert(Law<And>.isAssociative(a: false, b: false, c: true))
+        XCTAssert(Law<And>.isAssociative(a: true, b: false, c: true))
+    }
+
+    func test_or_isSemigroup() {
+        XCTAssert(Law<Or>.isAssociative(a: true, b: true, c: true))
+        XCTAssert(Law<Or>.isAssociative(a: false, b: false, c: false))
+
+        XCTAssert(Law<Or>.isAssociative(a: true, b: true, c: false))
+        XCTAssert(Law<Or>.isAssociative(a: true, b: false, c: false))
+
+        XCTAssert(Law<Or>.isAssociative(a: false, b: true, c: true))
+        XCTAssert(Law<Or>.isAssociative(a: false, b: true, c: false))
+
+        XCTAssert(Law<Or>.isAssociative(a: false, b: false, c: true))
+        XCTAssert(Law<Or>.isAssociative(a: true, b: false, c: true))
+    }
 }
